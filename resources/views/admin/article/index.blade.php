52abf6f44d31f748d83fcabcc6a4a9f7d8acadc5
Paul <horn.pan@gmail.com>
2017-04-24 17:47:48 +0800

    optimize

--- resources/views/admin/article/index.blade.php
+++ resources/views/admin/article/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin')
-
+@section('title', '市政爆料管理系统')
 @section('content')
 <div class="container">
     <div class="row">
@@ -19,7 +19,16 @@
                         <hr>
                         <div class="article">
                             <h6>
-                            
+                                <span><b>爆料人姓名：</b></span><span>
+                            @if($article->anonymous == 1)
+                                        匿名
+                                    @else
+                                        {{$article->name}}
+                                    @endif
+                            </span>&nbsp;
+                                <span><b>手机号：</b></span><span>{{$article->mobile}}</span>&nbsp;
+                                <span><b>地址：</b></span><span>{{$article->address}}</span>&nbsp;
+
                             </h6>
                             <div class="content">
                                 {{ $article->body }}
@@ -27,30 +36,21 @@
                                     $imgArray = explode(",", $article->images);
 //                                    var_dump($imgArray);
                                 ?>
-                                <p>
+                                <div >
                                     @foreach($imgArray as $img)
-                                        <a href="{{$img}}" rel="lightbox-{{$article->id}}}}"><img src="{{$img}}" class="cover_small"></a>
+                                        <a href="{{$img}}" rel="lightbox-{{$article->id}}}}"><img src="{{$img}}" class="cover_small" "></a>
                                     @endforeach
-
-                                </p>
+                                <form action="{{ url('admin/article/'.$article->id) }}" method="POST" style="display: inline; float:right; padding: 20px 10px;">
+                                    {{ method_field('DELETE') }}
+                                    {{ csrf_field() }}
+                                    <button type="submit" class="btn btn-danger">删除</button>
+                                </form>
+                                </div>
                             </div>
                         </div>
                         {{--<a href="{{ url('admin/article/'.$article->id.'/edit') }}" class="btn btn-success">编辑</a>--}}
                         <div style="font-size:9pt; color: #999;">
-                            <span><b>爆料人姓名：</b></span><span>
-                            @if($article->anonymous == 1)
-                                匿名
-                                @else
-                                    {{$article->name}}
-                                @endif
-                            </span>&nbsp;
-                            <span><b>手机号：</b></span><span>{{$article->mobile}}</span>&nbsp;
-                            <span><b>地址：</b></span><span>{{$article->address}}</span>&nbsp;
-                            <form action="{{ url('admin/article/'.$article->id) }}" method="POST" style="display: inline; float:right;">
-                                {{ method_field('DELETE') }}
-                                {{ csrf_field() }}
-                                <button type="submit" class="btn btn-danger">删除</button>
-                            </form>
+
                         </div>
 
                     @endforeach
