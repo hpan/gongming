5d2258f71020dc66deaf3773b41af4326f916978
Paul <Paul@hornpans-MacBook-Pro.local>
2017-08-02 12:23:04 +0800

    anoymous error

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -22,7 +22,7 @@
                                     <label style="float:right;font-weight: normal;">
                                         <input type="checkbox" name="anonymous[]" style="vertical-align:middle; margin-top: 0; margin-bottom: 1px;">
 
-                                        <span class="font-anonymous">我要匿名22</span>
+                                        <span class="font-anonymous">我要匿名</span>
                                     </label>
 
                             </div>
@@ -84,8 +84,8 @@
                         console.log(imgUrl);
                         console.log("image length=" + $("#image-holder > img").length );
                         if($("#image-holder > img").length >=3){
-                            console.log("最多只能传2张");
-                            alert("最多只能上传2张照片");
+                            console.log("最多只能传3张");
+                            alert("最多只能上传3张照片");
 //                        }else if($("#image-holder > img").length <=0){
 //                            alert("必须上传照片");
 //                            return false;
